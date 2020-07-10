2175a928dbbe0a54951e5dd667788058b83c91ab
Ignacio San Martin <ignaciosm@gmail.com>
2020-07-10 12:39:16 -0500

    ex3 mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,35 @@
+import math
+
+weights = [4, 6, 10, 15, 16]
+length = 5
+limit = 21
+
+# Your solution should run in linear time. SO IT'S OK TO LOOP THROUGH THE LIST
+
+
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    weights_dict = {}
+    index = -1
+
+    # create a hash table with key = weight and value = to index
+    # {4: 0, 6: 1, 10: 2, 15: 3, 16: 4}
+    for i in weights:
+        index += 1
+        weights_dict[i] = index
+        # print(weights_dict)
+
+    # loop through indexes in weights list and find index of complementary weight (if present)
+    for i1 in range(length):
+        w1 = weights[i1]
+        w2 = limit - weights[i1]
+        # get the index of the complement, if not present: None
+        i2 = weights_dict.get(w2)
+
+        # print(i2)
+        if i2 is not None:
+            return [max(i1, i2), min(i1, i2)]
 
     return None
+
+
+print(get_indices_of_item_weights(weights, length, limit))
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,13 +1,31 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    length = len(arrays)
+    combined_array = []
+    result = []
+
+    for i in arrays:
+        combined_array = combined_array + i
+
+    arr_dict = {}
+
+    for i in combined_array:
+        if i not in arr_dict:
+            arr_dict[i] = 1
+        else:  # already in dict
+            arr_dict[i] = arr_dict[i] + 1
+            if arr_dict[i] == length:
+                result.append(i)
 
     return result
 
 
 if __name__ == "__main__":
+    # arrays = [
+    #     [1, 2, 3, 4, 5],
+    #     [12, 7, 2, 9, 1],
+    #     [99, 2, 7, 1]
+    # ]
+
     arrays = []
 
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
